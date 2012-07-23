85dca4838d36fa8e8db240b308ebe03f9643160c
oss.request <oss.request@samsung.com>
2012-07-23 14:52:26 +0900

    NA_IC

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 4 14:44:33 KST 2012"
+#define LINUX_COMPILE_BY "uosarang"
+#define LINUX_COMPILE_HOST "Raptor"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "

f609ab474e38853d8b8ccb124b8ba5447ab81ae6
imoseyon <imoseyon@gmail.com>
2012-08-08 22:35:34 -0700

    build script change

--- buildlean.sh
+++ buildlean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [[ $2 == "compile" ]] &&
+  sed -i s/CONFIG_LOCALVERSION=\".*\"/CONFIG_LOCALVERSION=\"-leanKernel-${1}\"/ .config &&
   make ARCH=arm CROSS_COMPILE=/data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi- -j2
 find drivers -name "*.ko" | xargs /data/linaro/android-toolchain-eabi/bin/arm-linux-androideabi-strip --strip-unneeded
 find drivers -name "*.ko" | xargs -i cp {} zip/system/lib/modules/
@@ -12,5 +13,4 @@ cd ../
 /data/unpack-mkbootimg/mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=31' --base 0x80200000 --ramdiskaddr 0x81500000 --kernel arch/arm/boot/zImage --ramdisk /tmp/ramdisk.img -o zip/boot.img
 
 cd zip
-zip -r /tmp/lk_tw_testv${1}_r530.zip *
-[[ $2 == "upload" ]] && /data/utils/s3_ftpupload.sh $1
+zip -r /tmp/lk_tw_beta-v${1}_r530.zip *
